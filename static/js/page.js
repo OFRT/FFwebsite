b0c78c54369cea1e3d4206f3a44cac2ff6b0d1b3
ThuanGYQ <ThuanGYQ@users.noreply.github.com>
2020-09-24 19:31:10 +0800

    Update page.js

--- static/js/page.js
+++ static/js/page.js
@@ -8,7 +8,7 @@ layui.use('laytpl', function(laytpl){
   ,controller = function(){
       
       
-    var data_url = 'ofrt.top/tools/json/tools.json';
+    var data_url = 'ffweb.ofrt.top/data.json';
       
     // 获取json 数据 
     var json = "";
